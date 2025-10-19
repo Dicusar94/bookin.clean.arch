c0aefeeb8c10273cc0aedbf43706176700b68458
ddicusar <dicusar.dv@gmail.com>
2025-10-19 21:18:01 +0300

    feat: add ApplicationDbContexctDesignFactory

---
+++ src/Booking.Infrastructure/Persistence/ApplicationDbContextDesignFactory.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BookingApp.Persistence;
+
+public class ApplicationDbContextDesignFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        optionsBuilder.UseNpgsql(
+            "Host=localhost;Port=5432;Database=booking;Username=myuser;Password=mypassword",
+            b => b.MigrationsHistoryTable("__EFMigrationsHistory", "room-booking"));
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
