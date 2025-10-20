d3440a567a1effa7b84c31537ad29d42afc205af
ddicusar <dicusar.dv@gmail.com>
2025-10-20 09:41:00 +0300

    feat: add BookinPolicyService

---
+++ src/Booking.Infrastructure/DomainServices/Bookings/BookingPolicyService.cs
@@ -0,0 +1,52 @@
+using BookingApp.BookingAggregate;
+using BookingApp.BookingAggregate.Services;
+using BookingApp.RoomAggregate;
+using BookingApp.Telemetry;
+
+namespace BookingApp.DomainServices;
+
+public class BookingPolicyService(
+    IRoomRepository roomRepository, 
+    IBookingRepository bookingRepository) : IBookingPolicyService
+{
+    public async Task EnsureBookingCanBeCreatedAsync(Booking booking, CancellationToken ct = default)
+    {
+        var activity = RunTimeDiagnosticConfig.Source.StartActivity()
+            .SetBooking(booking);
+        
+        var room = await roomRepository.GetRoomById(booking.RoomId);
+
+        if (!room.IsAvailableOn(booking.Date, booking.TimeRange))
+        {
+            var exception = new Exception("Room not available during the requested time");
+            activity.AddExceptionAndFail(exception);
+            throw exception;
+        }
+
+        var overlappingBookings = await bookingRepository.GetOverlappingBookingsAsync(
+            booking.RoomId,
+            booking.Date,
+            booking.TimeRange,
+            ct);
+
+        if (overlappingBookings.Count >= room.Capacity)
+        {
+            var exception = new Exception($"Room capacity of {room.Capacity} exceeded for the requested time");
+            activity.AddExceptionAndFail(exception);
+            throw exception;
+        }
+
+        var userHasOverlaps = await bookingRepository.HasOverlappingUserBookingAsync(
+            booking.UserId,
+            booking.Date,
+            booking.TimeRange,
+            ct);
+
+        if (userHasOverlaps)
+        {
+            var exception = new Exception($"User already has overlapping bookings");
+            activity.AddExceptionAndFail(exception);
+            throw exception;
+        }
+    }
+}
