8d52d9645478e98a24346997bb7229dfa29aa383
ddicusar <dicusar.dv@gmail.com>
2025-11-01 15:53:58 +0200

    feat: refactor the booking policy service test

--- tests/Booking.Tests.Integration/DomainServices/BookingPolicyServiceTests.cs
+++ tests/Booking.Tests.Integration/DomainServices/BookingPolicyServiceTests.cs
@@ -1,52 +1,36 @@
 using BookingApp.BookingAggregate.Services;
 using BookingApp.Shared;
 using BookingApp.Utils;
+using BookingApp.Utils.TestContants.Rooms;
 using BookingApp.Utils.TestContants.Schared;
 using BookingApp.Utils.TestContants.Users;
 using Shouldly;
 
 namespace BookingApp.DomainServices;
 
-public class BookingPolicyServiceTests : IClassFixture<ApiFactory>, IAsyncLifetime
+public class BookingPolicyServiceTests(ApiFactory apiFactory) : IClassFixture<ApiFactory>, IAsyncLifetime
 {
-    private readonly IBookingPolicyService _sut;
-    private readonly IUnitOfWork _unitOfWork;
+    private readonly IBookingPolicyService _sut = apiFactory.GetService<IBookingPolicyService>();
+    private readonly IUnitOfWork _unitOfWork = apiFactory.GetService<IUnitOfWork>();
 
-    private Func<Task> _initDatabase;
-    private Func<Task> _resetDatabase;
-    
-    public BookingPolicyServiceTests(ApiFactory apiFactory)
-    {
-        _sut = apiFactory.GetService<IBookingPolicyService>();
-        _unitOfWork = apiFactory.GetService<IUnitOfWork>();
-        _initDatabase = apiFactory.TestDatabaseReset.InitializeAsync;
-        _resetDatabase = apiFactory.TestDatabaseReset.ResetAsync;
-
-    }
+    private readonly Func<Task> _initDatabase = apiFactory.TestDatabaseReset.InitializeAsync;
+    private readonly Func<Task> _resetDatabase = apiFactory.TestDatabaseReset.ResetAsync;
 
     [Fact]
     public async Task EnsureBookingCanBeCreatedAsync_should_be_valid()
     {
         // arrange
-        var room = TestRoomFactory.CreateRoom();
-        
-        var schedule = TestRoomFactory.CreateRecurringSchedule(
-            roomId: room.Id,
-            dayOfWeek: DateTimeConstants.DayOfWeek,
-            timeRange: TimeRangeConstants.EightAmToFivePm);
-        
-        room.AddSchedule(schedule);
-        await _unitOfWork.Rooms.AddRoom(room, CancellationToken.None);
-        await _unitOfWork.SaveChangesAsync(CancellationToken.None);
-
         var booking = TestBookingFactory.Create(
-            roomId: room.Id,
+            roomId: RoomConstants.Room1Id,
             userId: UserConstants.User1Id,
-            date: DateTimeConstants.DateNow,
+            date: DateTimeConstants.DateNow.AddDays(1),
+            timeRange: TimeRangeConstants.FourAmToFivePm,
             timeProvider: DateTimeConstants.TimeProvider);
         
         // act
         var action = async () => await _sut.EnsureBookingCanBeCreatedAsync(booking, CancellationToken.None);
+        
+        // assert
         await action.ShouldNotThrowAsync();
     }
 
